3d568e6c45d24b6c2e24042144caa328058a081b
dcgallo15 <dcgallo14@gmail.com>
2025-01-12 15:01:55 +0000

    Added suport for + and - operators

--- autoload/main.py
+++ autoload/main.py
@@ -1,5 +1,8 @@
 from ply import lex
 from ply import yacc
+import sys
+
+ret = 0
 
 tokens = [
     'INT',
@@ -22,26 +25,54 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-def tokenise(lexer, line: str):
-    # Tokenise:
-    tokens = []
-    lexer.input(line)
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break      # No more input
-        tokens.append(tok)
-    return tokens
+# will work after more ioerators are implemented
+#precedence = (('left', 'ADD', 'SUB')) # Mul and Div will go after
 
+ret = 0
+
+def p_calc(p):
+    ''' calc : expression
+        | empty '''
+    global ret
+    ret = run(p[1])
+
+def p_expression(p):
+    '''expression : expression ADD expression
+                | expression SUB expression'''
+
+    # Starting to generate syntax tree
+    p[0] = (p[2], p[1], p[3])
+
+def p_expression_int(p):
+    ''' expression : INT'''
+    p[0] = p[1]
+
+def p_empty(p):
+    ''' empty :'''
+    p[0] = None
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+def run(p):
+    if type(p) == tuple:
+        if p[0] == '+':
+            return p[1] + p[2]
+        elif p[0] == '-':
+            return p[1] - p[2]
+    else:
+        return p
 
 # This function will change the current line in the vim editor
 # Takes in expr to calculate
 def evalExpr(line: str):
+    global ret
     lexer = lex.lex()
-    tokens = tokenise(lexer, line)
-    print(tokens)
-    return "FUNCTION NOT YET FINISHED RETURN VAL OF evalExpr"
+    parser = yacc.yacc()
+    parser.parse(line)
+    return str(ret)
 
 if __name__ == "__main__":
-    print(evalExpr("5+5"))
+    print(evalExpr("12 + 24"))
 
